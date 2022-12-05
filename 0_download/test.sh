560cb434906b4a120f00cccd078c58cec0cbecaf
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-12-05 13:06:54 -0500

    rev

---
+++ 0_download/test.sh
@@ -0,0 +1,3 @@
+curl -O -b ~/.urs_cookies -c ~/.urs_cookies -L -n https://e4ftl01.cr.usgs.gov/MOLT/MOD09A1.061/2022.09.14/MOD09A1.A2022257.h00v08.061.2022266045506.hdf;
+curl -O -b ~/.urs_cookies -c ~/.urs_cookies -L -n https://e4ftl01.cr.usgs.gov/MOLT/MOD09A1.061/2022.09.14/MOD09A1.A2022257.h00v09.061.2022266045516.hdf;
+curl -O -b ~/.urs_cookies -c ~/.urs_cookies -L -n https://e4ftl01.cr.usgs.gov/MOLT/MOD09A1.061/2022.09.14/MOD09A1.A2022257.h00v10.061.2022266045243.hdf;
